0e3ee909e573ed5d84f275618727e47a76c0390e
Kshitij Pandey <74905584+KSP-07@users.noreply.github.com>
2023-01-22 07:30:12 +0530

    Update job-sequencing-problem.cpp

--- Job Sequencing Problem - GFG/job-sequencing-problem.cpp
+++ Job Sequencing Problem - GFG/job-sequencing-problem.cpp
@@ -33,23 +33,32 @@ class Solution
     vector<int> JobScheduling(Job arr[], int n) 
     { 
         // your code here
-        sort(arr , arr+n ,cmp);
         
+        //arr ko uske profit ke according sort kr rhe kuki max profit phle lena hai
+        sort(arr , arr+n , cmp);
+        
+        //ek array lerhe slots ki..kitne jobs le sktee hai
         bool done[n]={0};
         
+        //total kitni jobs krii aur profit kitna mila
         int jobs=0 , profit=0;
         
+        //array pe iterate kr rhee for n
         for(int i=0;i<n;i++){
-            for(int j= min(n , arr[i].dead -1) ; j>=0;j--){
+            //yaha pe j hum min lerhe n aur deadline mai kuki...deadline for ex 9 bhi hoskti..n sirf 5 hua...
+            //mtlb 9 wala aram se wait kr skta...pr slot to usko n ke size ke ander hii miljayga
+            //hum deadline ki max timing se 0th slot ki side chlke dekh rhe kuki for ex 2 thi deadline
+            //toh usko 2nd slot pe rkhdia...taaki first pe aur koi aa ske jiski deadline kam ho
+            for(int j= min(n , arr[i].dead -1);j>=0;j--){
                 if(done[j]==false){
-                    done[j]=true;
+                    jobs+=1;
                     profit+=arr[i].profit;
-                    jobs++;
+                    done[j]=true;    //slot milte hii true krke return krdenge
                     break;
                 }
             }
         }
-        return {jobs , profit};
+        return {jobs, profit};
     } 
 };
 
