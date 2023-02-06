b632c5997554bb98e18886e1341bfff66e8c7959
Kshitij Pandey <74905584+KSP-07@users.noreply.github.com>
2023-02-06 06:39:39 +0530

    Time: 3 ms (92.72%), Space: 9.9 MB (31.98%) - LeetHub

---
+++ 1470-shuffle-the-array/1470-shuffle-the-array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
+        int i=0 , j=n;
+        vector<int> ans;
+        while(i<nums.size() && j<nums.size()){
+            ans.push_back(nums[i++]);
+            ans.push_back(nums[j++]);
+        }
+        return ans;
+    }
+};
