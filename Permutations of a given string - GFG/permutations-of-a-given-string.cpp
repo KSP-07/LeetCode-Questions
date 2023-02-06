7eb371a680fa3554e626f033587dde6b67a108cc
Kshitij Pandey <74905584+KSP-07@users.noreply.github.com>
2023-02-06 08:03:07 +0530

    Added solution - LeetHub

--- Permutations of a given string - GFG/permutations-of-a-given-string.cpp
+++ Permutations of a given string - GFG/permutations-of-a-given-string.cpp
@@ -7,19 +7,66 @@ class Solution
 {
 public:
 		void func(string s , string out, vector<string>&ans){
+		    // to insert only unique output
 		    if(s.size()==0){
 		        if(find(ans.begin(), ans.end() ,out)==ans.end()){
 		            ans.push_back(out);
 		            return;
 		        }
 		    }
+		    
 		    for(int i=0;i<s.size();i++){
 		        char ch=s[i];
 		        string left = s.substr(0 ,i);
+		      //  cout<<ch<<" "<<left<<" ";
 		        string right = s.substr(i+1);
+		     //   cout<<right<<" ";
 		        string newS= left+right;
+		      //  cout<<newS<<endl;
 		        func(newS , out + ch ,ans);
+		      //  cout<<endl;
 		    }
+		    
+/*
+this is how it works:
+For Input: 
+ABC
+Your Output: 
+A  BC BC
+B  C C
+C  
+
+
+C B  B
+B  
+
+
+
+B A C AC
+A  C C
+C  
+
+
+C A  A
+A  
+
+
+
+C AB  AB
+A  B B
+B  
+
+
+B A  A
+A  
+
+
+
+ABC ACB BAC BCA CAB CBA
+
+*/
+		    
+		    
 		}
 		vector<string>find_permutation(string S)
 		{
