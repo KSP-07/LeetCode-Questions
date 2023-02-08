6a6e2b1fba1fe3343c324b142ae6e88fce73249d
Kshitij Pandey <74905584+KSP-07@users.noreply.github.com>
2023-02-08 07:38:40 +0530

    Time: 11 ms (95.23%), Space: 16.6 MB (56.17%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,3 +1,53 @@
+//RECURSIVE APPROACH
+// class Solution {
+// public:
+//     int jump(vector<int>& nums) {
+//         return helper(nums,0);
+//     }
+//     int helper(vector<int> &nums, int start){
+//         if(start >= nums.size()-1) return 0;
+        
+//         int minjump=100005;
+//         for(int i=start + 1 ; i<= start + nums[start] ; i++){
+//             minjump= min(minjump , 1 + helper(nums,i));
+//         }
+//         return minjump;
+//     }
+// };
+
+
+//TOP DOWN DP (MEMO)
+// 2. Memoization of recursive solution but it may give TLE.
+
+// class Solution {
+// public:
+ 
+//    long long int jump(vector<int> &nums , int curr, int dest,vector<int> &dp)
+//     {       
+//             if(curr==dest) return 0;
+//             if(dp[curr]!=-1) return dp[curr];
+//             //cout<<curr<<" ";
+//             long long int tmp=INT_MAX;
+     
+//             for(int i=1;i<=nums[curr];i++)
+//             {   if(i+curr>dest) break;
+//                 tmp=min(tmp,1+jump(nums,curr+i,dest,dp));  
+//             }
+//          dp[curr]=tmp;
+//          return tmp;  
+//     }
+    
+//     int jump(vector<int>& nums) {
+//          vector<int> dp(nums.size(),-1);
+//          return jump(nums,0,nums.size()-1,dp);
+//     }
+// };
+
+
+// 3. O(n) Solution (accepted)
+
+// We run loop from 0 to size()-1 because we have to reach at last index, think just previous stage when we take jump and reach to last index (size()-1) the we have counted our jump previously and we reach to last index hence we end our journey.
+
 class Solution {
 public:
     int jump(vector<int>& nums) {
