4b195682dbf7e95230fd9a6589d1b9064c0447e9
Kshitij Pandey <74905584+KSP-07@users.noreply.github.com>
2023-02-17 11:45:31 +0530

    Time: 0 ms (100.00%), Space: 9.8 MB (69.86%) - LeetHub

--- 0783-minimum-distance-between-bst-nodes/0783-minimum-distance-between-bst-nodes.cpp
+++ 0783-minimum-distance-between-bst-nodes/0783-minimum-distance-between-bst-nodes.cpp
@@ -12,22 +12,54 @@
 class Solution {
 public:
     
-    void diff(TreeNode* root , vector<int> &ans){
+//     void diff(TreeNode* root , vector<int> &ans){
+//         if(!root) return ;
+        
+//         diff(root->left , ans);
+//         ans.push_back(root->val);
+//         diff(root->right , ans);
+//     }
+    
+    
+//     int minDiffInBST(TreeNode* root) {
+//         int mini =INT_MAX;
+//         vector<int> ans;
+//         diff(root , ans);
+        
+//         for(int i=0;i<ans.size() - 1;i++){
+//             mini = min(mini , ans[i+1] - ans[i]);
+//         }
+//         return mini;
+//     }
+    
+    
+    //just by taking a previous pointer
+    
+    void diff(TreeNode* root , int &mini , int &prev){
+        
         if(!root) return ;
         
-        diff(root->left , ans);
-        ans.push_back(root->val);
-        diff(root->right , ans);
-    }
-    
-    int minDiffInBST(TreeNode* root) {
-        int mini =INT_MAX;
-        vector<int> ans;
-        diff(root , ans);
+        //traversing towards left
+        diff(root->left , mini, prev);
         
-        for(int i=0;i<ans.size() - 1;i++){
-            mini = min(mini , ans[i+1] - ans[i]);
+        //checking diff with prev pointer and then moving prev to current root
+        if(prev!=-1){
+            mini = min(mini , (root->val - prev));
         }
+        prev = root->val;
+        
+        //traversing towards right
+        diff(root->right , mini , prev);
+        
+        
+    }
+    int minDiffInBST(TreeNode* root){
+        int mini = INT_MAX;
+        int prev =-1;
+        
+        diff(root ,  mini , prev);
+        
         return mini;
     }
+    
 };
