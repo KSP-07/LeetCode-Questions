fc9aba21e39bfad45cff6e1afe6cf9cb3639369c
Kshitij Pandey <74905584+KSP-07@users.noreply.github.com>
2023-02-21 07:25:08 +0530

    Time: 32 ms (43.92%), Space: 22.3 MB (62.74%) - LeetHub

---
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        if(n==1) return nums[0];
+        int low = 0 , high = n-1;
+        
+        while(low<high){
+            int mid = low + (high-low)/2;
+            
+            if(mid%2==1) mid--;
+            if(nums[mid] != nums[mid+1]) high=mid;
+            else low = mid+2;
+        }
+        return nums[low];
+    }
+};
+
+// class Solution {
+// public:
+//     int singleNonDuplicate(vector<int>& nums) {
+//         int n = nums.size();
+//         int curr_xor=0; //variable will store XOR
+//         for(int i =0;i<n;i++){
+//             curr_xor ^= nums[i]; // XOR
+//         }
+//         return curr_xor;
+//     }
+// };
