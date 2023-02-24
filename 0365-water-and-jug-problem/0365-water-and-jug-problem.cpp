43b468ad848a14e186220b4d69626e68e0885912
Kshitij Pandey <74905584+KSP-07@users.noreply.github.com>
2023-02-24 14:43:54 +0530

    Time: 37 ms (42.16%), Space: 16.6 MB (43.43%) - LeetHub

--- 0365-water-and-jug-problem/0365-water-and-jug-problem.cpp
+++ 0365-water-and-jug-problem/0365-water-and-jug-problem.cpp
@@ -2,11 +2,41 @@ class Solution {
 public:
     bool canMeasureWater(int x, int y, int t) {
         //math solution
+        /*
         if(x+y ==t || x==t || y==t) return true;
         if(x+y<t) return false;
         
         int d = gcd(x, y);
         // cout<<t<<" "<<d<<endl;
         return (t%d==0);
+        */
+        
+        //bfs approach
+        if(x+y ==t || x==t || y==t) return true;
+        if(x+y<t) return false;
+        
+        int max_cap = x + y;
+        queue<int> q;
+        q.push(0);
+        
+        vector<int> vis(max_cap+1 , 0);
+        vis[0]=1;
+        vector<int> move= {x , -x , y ,-y};
+        while(!q.empty()){
+            int curr = q.front();
+            q.pop();
+            
+            if(curr == t) return true;
+            
+            for(auto i:move){
+                int k = i + curr;
+                
+                if(k>=0 && k<=max_cap && !vis[k]){
+                    q.push(k);
+                    vis[k]=1;
+                }
+            }
+        }
+        return false;
     }
 };
