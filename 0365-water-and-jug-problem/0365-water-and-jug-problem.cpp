3f73f44d7ad1fad87c16e99a6929813f810dea8f
Kshitij Pandey <74905584+KSP-07@users.noreply.github.com>
2023-02-24 14:45:15 +0530

    Time: 44 ms (36.12%), Space: 16.7 MB (42.80%) - LeetHub

--- 0365-water-and-jug-problem/0365-water-and-jug-problem.cpp
+++ 0365-water-and-jug-problem/0365-water-and-jug-problem.cpp
@@ -1,5 +1,22 @@
 class Solution {
 public:
+    //dfs app
+    /*
+   bool dfs(int x, int y, int z, int m, int curr, vector<int> &vis)
+    {
+        if (curr < 0 || curr > m || vis[curr] == 1)
+            return false;
+        if (curr == z)
+            return true;
+        vis[curr] = 1;
+        bool a = dfs(x, y, z, m, curr + x, vis);
+        bool b = dfs(x, y, z, m, curr - x, vis);
+        bool c = dfs(x, y, z, m, curr + y, vis);
+        bool d = dfs(x, y, z, m, curr - y, vis);
+        return a || b || c || d;
+    }
+    
+    */
     bool canMeasureWater(int x, int y, int t) {
         //math solution
         /*
@@ -38,5 +55,16 @@ public:
             }
         }
         return false;
+        
+        /*
+        DFS APPROACH
+         if (x + y == z || x == z || y == z)
+            return true;
+        if (x + y < z)
+            return false;
+        int m = x + y; // max capacity
+        vector<int> vis(m + 1, 0);
+        return dfs(x, y, z, m, 0, vis);
+        */
     }
 };
