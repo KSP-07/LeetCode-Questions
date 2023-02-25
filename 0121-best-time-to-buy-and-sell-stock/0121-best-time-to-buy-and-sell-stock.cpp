fd33809845dd2b8d33c8fdd25a1c01315a1597b4
Kshitij Pandey <74905584+KSP-07@users.noreply.github.com>
2023-02-25 06:02:49 +0530

    Time: 131 ms (84.35%), Space: 93.4 MB (11.82%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,10 +1,6 @@
 class Solution {
 public:
-//     void solve(){
-//         if(bought==0){
-            
-//         }
-//     }
+/*
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
         int stock = prices[n-1];
@@ -15,5 +11,19 @@ public:
             stock = max(stock , temp);
         }
         return maxi;
+    }
+    */
+        int maxProfit(vector<int>& prices) {
+        int n= prices.size();
+        int minVal=prices[0] , maxProfit=0;
+        for(int i=1;i<n;i++){
+            if(minVal>prices[i]) minVal=prices[i];
+            
+            
+            //since max=0 , ans will not go -ve , and we are checking for prices[i]-min coz we are looking for max profit in the array we could get with curr min
+            
+            else if(maxProfit< prices[i]-minVal) maxProfit= prices[i]-minVal;
+        }
+        return maxProfit;
     }
 };
