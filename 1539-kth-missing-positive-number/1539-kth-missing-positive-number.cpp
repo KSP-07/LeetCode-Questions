b7278ec37300ffd8c22f78cab19861098240c3a5
Kshitij Pandey <74905584+KSP-07@users.noreply.github.com>
2023-03-06 08:32:27 +0530

    Time: 3 ms (88.86%), Space: 9.6 MB (73.33%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
@@ -1,18 +1,34 @@
-class Solution {
+// class Solution {
+// public:
+//     int findKthPositive(vector<int>& arr, int k) {
+//         unordered_map<int,int> mp;
+//         for(auto i:arr) mp[i]++;
+        
+//         int i=1 ,ans=0;
+//         while(k){
+//             if(mp[i]) i++;
+//             else{
+//                 ans=i;
+//                 i++;
+//                 k--;
+//             }
+//         }
+//         return ans;
+//     }
+// };
+    
+    class Solution {
 public:
     int findKthPositive(vector<int>& arr, int k) {
-        unordered_map<int,int> mp;
-        for(auto i:arr) mp[i]++;
-        
-        int i=1 ,ans=0;
-        while(k){
-            if(mp[i]) i++;
-            else{
-                ans=i;
-                i++;
-                k--;
-            }
+        int n = arr.size();
+        int start = 0, end = n-1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] - (mid + 1) < k)
+                start = mid + 1;
+            else
+                end = mid - 1;
         }
-        return ans;
+        return start + k;
     }
 };
