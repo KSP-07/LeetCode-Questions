64e0512af5c63e7dca2d28f88f55d2a09c7497e0
Kshitij Pandey <74905584+KSP-07@users.noreply.github.com>
2023-03-10 02:15:21 +0530

    Added solution - LeetHub

---
+++ Division without using multiplication, division and mod operator - GFG/division-without-using-multiplication-division-and-mod-operator.cpp
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+    public:
+    long long divide(long long a, long long b) 
+    {
+        //First step: COnvert the negative number to a positive one.
+        
+        int sign= ((a<0) ^ (b<0))?-1:1;
+        long long dividend=abs(a);
+        long long divisor=abs(b);
+        
+        long long temp=0;
+        long long quotient=0;
+        // Step two iterate all the 32 bits to find the best power of 2 that satisfies the division
+        //NOTE: If in question -10^9<=a<=10^9 then the no of bits is 32, for-10^18<=a<=10^18 the no of bits is 64
+        for(int i=31;i>=0;i--)
+        {
+            if(temp+ (divisor<<i)<=dividend)
+            {
+                temp+=divisor<<i;
+                quotient+=(1LL<<i);
+            }
+        }
+        
+        return quotient*sign;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+	int t;
+	cin >> t;
+
+	while (t--)
+	{
+
+		long long a, b;
+		cin >> a >> b;
+		
+		Solution ob;
+		cout << ob.divide(a, b) << "\n";
+	}
+
+	return 0;
+}
+
+// } Driver Code Ends
