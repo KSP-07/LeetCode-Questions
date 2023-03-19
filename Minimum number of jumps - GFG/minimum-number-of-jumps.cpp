817f8295f6dcbaf5f2c7eeba73794973b6bd633f
Kshitij Pandey <74905584+KSP-07@users.noreply.github.com>
2023-03-19 23:02:48 +0530

    Added solution - LeetHub

--- Minimum number of jumps - GFG/minimum-number-of-jumps.cpp
+++ Minimum number of jumps - GFG/minimum-number-of-jumps.cpp
@@ -8,24 +8,26 @@ using namespace std;
 
 class Solution{
   public:
+  //using greedy approach
+  //ek element pe khde hoke dekh lenge ki waha se kaha tkk jaaskte , aur jaake max dekh lenge uske aage ka , 
+  //fir jab ek baar reach khtm hojaygi toh fir max wali jump laga denge
     int minJumps(int arr[], int n){
         // Your code here
-        if(arr[0]==0) return -1;
-        int steps=arr[0];
-        int maxReach=arr[0];
-        int jump=1;
-
-        for(int i=1;i<n-1;i++){
-            steps--;
-            maxReach=max(maxReach , arr[i] + i);
-            
-            if(steps==0){
-                jump++;
-                steps=maxReach-i;
-                if(steps<=0) return -1;
+        //base case
+        if(arr[0]==0 && n>1) return -1;
+        int currmove = arr[0];
+        int maxMove = currmove;
+        int steps = 1;
+        for(int i=1 ; i< n-1; i++){
+            currmove--;
+            maxMove = max(maxMove , arr[i] + i);
+            if(currmove == 0) {
+                currmove = maxMove - i; //ab itna aage aur move kr sktee
+                if(currmove <= 0) return -1;   // agar 0 aata hai as a element then  , maxmove -i 0 hoskta ya -ve , iska mtlb ab move nhi kr skte
+                steps++;
             }
         }
-        return jump;
+        return steps;
     }
 };
 
