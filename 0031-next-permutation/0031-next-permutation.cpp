8e1678ebdd360d88e3feba217f3781059eb68090
Kshitij Pandey <74905584+KSP-07@users.noreply.github.com>
2023-03-21 00:35:28 +0530

    Time: 9 ms (12.07%), Space: 12.1 MB (76.50%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,7 +1,9 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
+        
         int n = nums.size();
+        //finding a breakpoint index , i.e if from right to left array is not increasing at some point
         int ind=-1;
         for(int i=n-1 ;i>0;i--){
             if(nums[i] > nums[i-1]){
@@ -10,7 +12,9 @@ public:
             }
         }
         // cout<<ind<<endl;
+        //this means that we have found one point
         if(ind!=-1){
+            //not traversing from back to find element just greater than on the ind element and then swap it
             for(int i=n-1;i>ind;i--){
                 if(nums[i]>nums[ind]){
                     // cout<<nums[i]<<" "<<nums[ind]<<endl;
@@ -19,6 +23,7 @@ public:
                 }
             }
         }
+        //now after the breakpoint reversing the array to make all thing in smallest order after breakpoint
         reverse(nums.begin() + ind+1 , nums.end() );
     }
 };
