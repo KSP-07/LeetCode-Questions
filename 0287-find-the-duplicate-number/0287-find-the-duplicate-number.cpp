46c4da80f4608ea95fcc066d7f1e90935be77405
Kshitij Pandey <74905584+KSP-07@users.noreply.github.com>
2023-03-20 22:40:20 +0530

    Time: 96 ms (97.61%), Space: 61.2 MB (67.09%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,15 +1,41 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        for(int i=0;i<nums.size();i++){
-            if(nums[abs(nums[i])]>=0){
-                nums[abs(nums[i])] = -nums[abs(nums[i])];
+        //inversion of elemenets approach
+        /*
+        int duplicate = -1;
+        for (int i = 0; i < nums.size(); i++) {
+            int cur = abs(nums[i]);
+            if (nums[cur] < 0) {
+                duplicate = cur;
+                break;
             }
-            else{
-                //means it was already marked -ve by someone so repeat caught
-                return abs(nums[i]);
-            }
+            nums[cur] *= -1;
         }
-        return -1;
+        
+        // Restore numbers
+        for (auto& num : nums)
+            num = abs(num);
+        
+        return duplicate;
+        */
+        
+        //tortoise and hare approach for cycle detection
+        int tortoise = nums[0];
+        int hare = nums[0];
+        
+        do{
+            tortoise = nums[tortoise];
+            hare = nums[nums[hare]];
+        }while(hare!=tortoise); 
+        
+        //now making hare start again at normal pace
+        hare = nums[0];
+        while(hare != tortoise){
+            hare = nums[hare];
+            tortoise = nums[tortoise];
+        }
+        
+        return hare;
     }
 };
