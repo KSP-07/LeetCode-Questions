67fd5fe1ed372b83033d02fc1a1f9463665ea1f1
Kshitij Pandey <74905584+KSP-07@users.noreply.github.com>
2023-03-28 11:09:36 +0530

    Update 0123-best-time-to-buy-and-sell-stock-iii.cpp

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,6 +1,7 @@
-class Solution {
-public:
-    int solve(int index , int buy , vector<int>& prices , int limit , vector<vector<vector<int>>> &dp){
+/*
+bottom up dp solution
+
+int solve(int index , int buy , vector<int>& prices , int limit , vector<vector<vector<int>>> &dp){
     if(index>=prices.size()) return 0;
     if(limit<=0) return 0;
     if(dp[index][buy][limit]!= -1) return dp[index][buy][limit];
@@ -17,10 +18,44 @@ public:
     }
     return dp[index][buy][limit] = profit;
 }
+*/
+
+int solveTab(vector<int> &prices){
+    int n = prices.size();
+
+    // vector<vector<vector<int>>> dp(n+1 , vector<vector<int>> (2 , vector<int>(3 , 0)));
+    
+    vector<vector<int>> curr(2 , vector<int> (3 ,0));
+    vector<vector<int>> next(2 , vector<int>(3 ,0));
+    
+    for(int index = n-1; index>=0 ; index--){
+        for(int buy = 0 ; buy <= 1 ; buy++){
+            for(int limit =1; limit<=2 ; limit++){
+                int profit = 0;
+                if(buy){
+                    int buykaro = -prices[index] +  next[0][limit];
+                    int skipkaro = 0 +  next[1][limit];
+                    profit = max(buykaro  , skipkaro);
+                }
+                else{
+                    int sellkaro = prices[index] + next[1][limit -1];
+                    int skipkaro = 0 + next[0][limit];
+                    profit = max(sellkaro , skipkaro);
+                }
+                curr[buy][limit] = profit;
+            }
+            next = curr;
+        }
+    }
+    // return dp[0][1][2];
+    
+    return next[1][2];
+}
+
 int maxProfit(vector<int>&price){
     //Write your code here..
     int n = price.size();
-    vector<vector<vector<int>>> dp(n , vector<vector<int>> (2 , vector<int>(3 , -1)));
-    return solve(0,1, price , 2 , dp);
+    // vector<vector<vector<int>>> dp(n , vector<vector<int>> (2 , vector<int>(3 , -1)));
+    // return solve(0,1, price , 2 , dp);
+    return solveTab(price);
 }
-};
