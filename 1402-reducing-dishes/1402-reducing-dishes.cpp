83e602719e409d06e9eef0a5a0a66aaad4b7b302
Kshitij Pandey <74905584+KSP-07@users.noreply.github.com>
2023-03-29 09:47:29 +0530

    Time: 65 ms (9.45%), Space: 21 MB (28.46%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.cpp
+++ 1402-reducing-dishes/1402-reducing-dishes.cpp
@@ -1,5 +1,8 @@
 class Solution {
 public:
+    /* 
+    Memoization
+    
     int solve(vector<int> &arr , int i , int index, int n , vector<vector<int>> &dp ){
         if(i>=n ) return 0;
         if(dp[i][index] !=-1) return dp[i][index] ;
@@ -19,5 +22,23 @@ public:
         
         vector<vector<int>> dp(n+1, vector<int> (n+1,-1));
         return solve(arr , 0 , 1 , n , dp);
+    }
+    */
+    
+    //tabulation
+    int maxSatisfaction(vector<int>& satisfaction) {
+        sort(satisfaction.begin(), satisfaction.end());
+        
+        // Mark all the states initially as 0.
+        vector<vector<int>> dp(satisfaction.size() + 1, vector<int>(satisfaction.size() + 2, 0));
+        for (int index = satisfaction.size() - 1; index >= 0; index--) {
+            for (int time = 1; time <= satisfaction.size(); time++) {
+                // Maximum of two choices:
+                // 1. Cook the dish at `index` with the time taken as `time` and move on to the next dish with time as time + 1.
+                // 2. Skip the current dish and move on to the next dish at the same time.
+                dp[index][time] = max(satisfaction[index] * time + dp[index + 1][time + 1], dp[index + 1][time]);
+            }
+        }
+        return dp[0][1];
     }
 };
