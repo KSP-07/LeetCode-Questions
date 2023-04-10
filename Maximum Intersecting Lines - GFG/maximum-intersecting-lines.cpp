e5df46d3d0fae86cb2a98caaf4dc0d9a9446d761
Kshitij Pandey <74905584+KSP-07@users.noreply.github.com>
2023-04-10 13:30:09 +0530

    Added solution - LeetHub

--- Maximum Intersecting Lines - GFG/maximum-intersecting-lines.cpp
+++ Maximum Intersecting Lines - GFG/maximum-intersecting-lines.cpp
@@ -12,16 +12,41 @@ class Solution {
   public:
     int maxIntersections(vector<vector<int>> lines, int N) {
         // Code here
-        map<int ,int> mp;
-        for(vector<int> line : lines){
-            int start = line[0]  , end = line[1] + 1;
-            mp[start]++;
-            mp[end]--;
+        // map<int ,int> mp;
+        // for(vector<int> line : lines){
+        //     int start = line[0]  , end = line[1] + 1;
+        //     mp[start]++;
+        //     mp[end]--;
+        // }
+        // int res =0 , cnt = 0;
+        // for(auto i: mp){
+        //     cnt += i.second;
+        //     res = max(res , cnt);
+        // }
+        // return res;
+        
+        vector<int> v1 , v2;
+        
+        for(auto i: lines){
+            v1.push_back(i[0]);
+            v2.push_back(i[1]);
         }
-        int res =0 , cnt = 0;
-        for(auto i: mp){
-            cnt += i.second;
-            res = max(res , cnt);
+        
+        sort(v1.begin() , v1.end());
+        sort(v2.begin() , v2.end());
+        
+        int i = 0 , j = 0;
+        int cnt = 0  , res =0;
+        while(i<N){
+            if(v1[i] <= v2[j]){
+                cnt++;
+                i++;
+                res = max(res , cnt);
+            }
+            else{
+                j++;
+                cnt--;
+            }
         }
         return res;
     }
