558038af2f591c0b2e055bba9ace14eae2df90e7
Kshitij Pandey <74905584+KSP-07@users.noreply.github.com>
2023-04-12 08:34:47 +0530

    Time: 13 ms (11.27%), Space: 10.7 MB (14.23%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        string ans ="";
+        stack<string> st;
+        int n = path.size();
+        
+        for(int i =0; i< n ;i++){
+            if(path[i]=='/') continue;
+            
+            string temp="";
+            while(i< n && path[i] !='/'){
+                temp.push_back(path[i]);
+                i++;
+            }
+            if(temp == ".") continue;
+            if(temp==".."){
+                if(!st.empty()) st.pop();
+            }
+            else st.push(temp);
+        }
+        while(!st.empty()){
+            ans = '/' + st.top() + ans;
+            st.pop();
+        }
+        if(ans.size()==0) return "/";
+        
+        return ans;
+    }
+};
