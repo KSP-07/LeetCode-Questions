8f394653213151c6f293dcd93dfcbab99d88c38a
Kshitij Pandey <74905584+KSP-07@users.noreply.github.com>
2023-04-13 11:38:12 +0530

    Update prerequisite-tasks.cpp

--- Prerequisite Tasks - GFG/prerequisite-tasks.cpp
+++ Prerequisite Tasks - GFG/prerequisite-tasks.cpp
@@ -3,52 +3,89 @@
 using namespace std;
 
 // } Driver Code Ends
+// class Solution {
+// public:
+// //this qstn is same as Minimum time taken by each job
+// 	bool isPossible(int n, vector<pair<int, int> >& pre) {
+// 	    // Code here
+	    
+	    
+// 	    vector<int> adj[n];
+// 	    vector<int> in(n , 0);
+	    
+// 	    for(auto i:pre){
+// 	        adj[i.first].push_back(i.second);
+// 	        in[i.second]++;
+// 	    }
+	    
+// 	   // for(int i=0;i<n;i++){
+// 	   //     for(int it:adj[i])
+// 	   //         in[it]++;
+// 	   // }
+	    
+// 	    queue<int> q;
+	    
+// 	    for(int i=0;i<n;i++){
+// 	        if(in[i]==0) q.push(i);
+// 	    }
+// 	    if(q.empty()) return false;
+	    
+// 	    int cnt=0;
+// 	    while(!q.empty()){
+// 	        int node= q.front();
+// 	       // int steps = q.front().second;
+// 	        q.pop();
+	        
+// 	        cnt++;
+	        
+// 	        for(auto i: adj[node]){
+// 	            in[i]--;
+// 	            if(in[i]==0){
+// 	                q.push(i);
+// 	            }
+// 	        }
+// 	    }
+
+// 	  return cnt==n;
+// 	}
+// };
+
+
 class Solution {
 public:
-//this qstn is same as Minimum time taken by each job
-	bool isPossible(int n, vector<pair<int, int> >& pre) {
+	bool isPossible(int N, vector<pair<int, int> >& prerequisites) {
 	    // Code here
 	    
+	    vector<int> indegree(N) , adj[N];
 	    
-	    vector<int> adj[n];
-	    vector<int> in(n , 0);
-	    
-	    for(auto i:pre){
-	        adj[i.first].push_back(i.second);
-	        in[i.second]++;
+	    for(auto i : prerequisites){
+	        adj[i.second].push_back(i.first);
+	        indegree[i.first]++;
 	    }
-	    
-	   // for(int i=0;i<n;i++){
-	   //     for(int it:adj[i])
-	   //         in[it]++;
-	   // }
 	    
 	    queue<int> q;
-	    
-	    for(int i=0;i<n;i++){
-	        if(in[i]==0) q.push(i);
+	    for(int i= 0 ; i<N;i++){
+	        if(indegree[i] == 0) q.push(i);
 	    }
-	    if(q.empty()) return false;
-	    
-	    int cnt=0;
+	    int cnt = 0;
 	    while(!q.empty()){
-	        int node= q.front();
-	       // int steps = q.front().second;
-	        q.pop();
+	        int node = q.front(); q.pop();
 	        
-	        cnt++;
-	        
-	        for(auto i: adj[node]){
-	            in[i]--;
-	            if(in[i]==0){
-	                q.push(i);
+            cnt++;
+	        for(auto v : adj[node]){
+	            indegree[v]--;
+	            if(indegree[v]==0){
+	                q.push(v);
 	            }
 	        }
 	    }
-
-	  return cnt==n;
+	    
+	    if(cnt==N) return true;
+	    
+	    return false;
 	}
 };
+
 
 //{ Driver Code Starts.
 int main(){
