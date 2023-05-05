f4c8c866709ce966ff1482d2f9fb42dca5db3d61
Kshitij Pandey <74905584+KSP-07@users.noreply.github.com>
2023-05-05 21:30:09 +0530

    Added solution - LeetHub

---
+++ Longest subsequence-1 - GFG/longest-subsequence1.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+
+    int solve(int i, int last , int n , int a[]){
+        if(i==n) return 0;
+        
+        int pick = 0;
+        if(last==-1 || abs(a[last] - a[i]) == 1){
+            pick = 1 + solve(i+1 , i , n , a);
+        }
+        int nopick = solve(i+1 , last , n , a);
+        return max(pick , nopick);
+    }
+
+//why sorting would not work> -> take this test case for example...[4 , 5 , 4 , 5 , 4, 5 , 4 , 5] + sorting -> 44445555
+    int longestSubsequence(int N, int A[])
+    {
+        // code here
+        // int cnt = 1;
+        return solve( 0 , -1, N , A);
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        int A[N];
+        for(int i = 0;i < N;i++)
+            cin>>A[i];
+        
+        Solution ob;
+        cout<<ob.longestSubsequence(N, A)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
