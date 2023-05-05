afade197ad406bf69f11ad35ab93d4b507405ac3
Kshitij Pandey <74905584+KSP-07@users.noreply.github.com>
2023-05-05 07:58:22 +0530

    Time: 51 ms (100.00%), Space: 41.5 MB (100.00%) - LeetHub

---
+++ 2667-create-hello-world-function/2667-create-hello-world-function.js
@@ -0,0 +1,14 @@
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    return function(...args) {
+        // console.log("Hello World");
+        return "Hello World";
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
