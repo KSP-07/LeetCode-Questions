176fb0c3b5ba92ab5698ea5b120550d386268bde
Kshitij Pandey <74905584+KSP-07@users.noreply.github.com>
2023-05-15 15:07:07 +0530

    Time: 2632 ms (5.17%), Space: 30.1 MB (10.00%) - LeetHub

--- 1799-maximize-score-after-n-operations/1799-maximize-score-after-n-operations.cpp
+++ 1799-maximize-score-after-n-operations/1799-maximize-score-after-n-operations.cpp
@@ -1,36 +1,48 @@
-class Solution 
-{
-    public:
-    int dp[1<<14];
-    int gcd(int num1,int num2)
+// https://leetcode.com/problems/maximize-score-after-n-operations/discuss/3521639/Recursion-greaterBoolean-Array-greaterMask-oror-Easy-to-understand-oror-Simple-Code
+
+class Solution {
+public:
+    
+    int solve(vector<int>&nums,  unordered_map<vector<bool>, int>&mp, vector<bool>&visited, int operation)
     {
-        if(num2==0) return num1;
-        return gcd(num2,num1%num2);
-    }
-    int dfs(vector<int>& nums,int state,int current)
-    {
-        if(current==0) return 0;
-        if(dp[state]) return dp[state];
-        int result=0,n=nums.size();
-        
-        vector<int> total;
-		
-		//remaining element
-        for(int i=0;i<n;i++)
-            if(state&(1<<i))
-                total.push_back(i);
-				
-        
-        for(int i=0;i<total.size();i++)
-            for(int j=i+1;j<total.size();j++)
-                result=max(result,gcd(nums[total[i]],nums[total[j]])*current+dfs(nums,(state&(~((1<<total[i])|(1<<total[j])))),current-1));
-        
-        dp[state]=result;
-        return result;
+        if (mp.count(visited)) return mp[visited]; //use stored result
+            
+        int maxScore = 0;
+        for (int i = 0; i < nums.size() - 1; i++)
+        {
+            if (visited[i]) continue;
+            for (int j = i + 1; j < nums.size(); j++)
+            {
+                if (visited[j]) continue;
+                visited[i] = true;
+                visited[j] = true;
+                //=====================================================================
+                int currScore = operation * __gcd(nums[i], nums[j]);
+                int nextMaxScore = solve(nums, mp, visited, operation + 1);
+                int totalScore = currScore + nextMaxScore;
+                maxScore = max(maxScore, totalScore);
+                //======================================================================
+                visited[i] = false;
+                visited[j] = false;
+            }
+        }
+        return mp[visited] = maxScore; //store the result
     }
     int maxScore(vector<int>& nums) 
     {
-        memset(dp,0,sizeof(dp));
-        return dfs(nums,(1<<14)-1,nums.size()/2);
+        int n = nums.size();
+        vector<bool>visited(n, false);
+        unordered_map<vector<bool>, int>mp;
+        int ans = solve(nums, mp, visited, 1);
+        
+        
+//         for(auto i: mp){
+//             for(auto j : i.first){
+//                 cout<<j<<" ";
+//             }
+//             cout<<"   "<<i.second<<endl;
+//         }
+        return ans;
+        
     }
 };
