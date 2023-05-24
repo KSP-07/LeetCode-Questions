77d60be2f720c7c90b50bd01c260ce462c8d9be9
Kshitij Pandey <74905584+KSP-07@users.noreply.github.com>
2023-05-24 20:19:53 +0530

    Time: 329 ms (30.54%), Space: 92.1 MB (33.01%) - LeetHub

--- 2542-maximum-subsequence-score/2542-maximum-subsequence-score.cpp
+++ 2542-maximum-subsequence-score/2542-maximum-subsequence-score.cpp
@@ -14,8 +14,10 @@ public:
             curr+=v[i].second;
             pq.push(v[i].second);
             if(i>=k-1){
-                res=max(res,curr*v[i].first);
-                curr-=pq.top();
+                res=max(res,curr*v[i].first);  //yaha pe desc order mai hai, to k ki value ke hisab se max among min aajayag
+                curr-=pq.top();   //agar nums1 mai koi bddi value hi bhut, jo ki multiply krne se bhi jada ho...to wo to reh jaygii naa, fir chahe chote num se ku na multiply horha ho baad mai
+                
+                //or ofcourse, agr hum nums2 ke min ki side jarhe, toh hum apna nums 1 ka sum toh max hii rkhna chahege
                 pq.pop();
             }
             i++;
