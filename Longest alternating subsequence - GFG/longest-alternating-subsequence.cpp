da8657f300df9752825a770b6712f6ec351ba01a
Kshitij Pandey <74905584+KSP-07@users.noreply.github.com>
2023-06-12 17:07:45 +0530

    Added solution - LeetHub

--- Longest alternating subsequence - GFG/longest-alternating-subsequence.cpp
+++ Longest alternating subsequence - GFG/longest-alternating-subsequence.cpp
@@ -5,43 +5,44 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
 	public:
+	
+	/*
+	Dynamic Programming solution(Longest Increasing Subsequence extension)
 		int AlternatingaMaxLength(vector<int>&nums){
 		    // Code here
 		    int n = nums.size();
+		    vector<vector<int>> las(n , vector<int> (2 , 1));
 		    
-		    /*
-		    DP n^2
-		    vector<vector<int>> dp(n , vector<int> (2,1));
+		    int res = 1 ;
 		    
-		    int res = 1;
-		    
-		    for(int i = 1 ; i<n ;i++){
+		    for(int i = 1 ; i < n ; i++){
 		        for(int j = 0 ; j < i ; j++){
-		            if(nums[j] < nums[i] && dp[i][0] < dp[j][1] +1 ){
-		                dp[i][0] = dp[j][1] +1;
+		            if(nums[j] < nums[i] && las[i][1] < las[j][0] +1 ){
+		                las[i][1] = las[j][0] + 1 ;
 		            }
-		            else if(nums[j] > nums[i] && dp[i][1] < dp[j][0] + 1){
-		                dp[i][1] = dp[j][0] + 1;
+		            else if(nums[j] > nums[i] && las[i][0] < las[j][1] + 1){
+		                las[i][0] = las[j][1] + 1 ;
 		            }
 		        }
-    		    res = max(dp[i][0] , dp[i][1]);
+		        res = max(res , max(las[i][0] , las[i][1]));
 		    }
-		    
 		    return res;
-		    
-		    */
-		    
-		    
-		    int inc = 1 , dec = 1;
-		    int res = 1;
-		    
+		}
+		
+		*/
+		
+		int AlternatingaMaxLength(vector<int>&arr){
+		    int inc =1 , dec = 1;
+		    int n = arr.size();
 		    for(int i = 1 ; i<n ; i++){
-		        if(nums[i-1] < nums[i]) inc = dec+1;
-		        else if(nums[i-1] > nums[i]) dec = inc+1;
-		        res = max(inc , dec);
- 		    }
- 		    
- 		    return res;
+		        if(arr[i] > arr[i-1]){
+		            inc = dec + 1 ;
+		        }
+		        else if(arr[i] < arr[i-1]){
+		            dec = inc + 1 ;
+		        }
+		    }
+		    return max(inc , dec );
 		}
 
 };
