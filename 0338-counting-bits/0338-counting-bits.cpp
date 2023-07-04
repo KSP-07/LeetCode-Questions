422cde848ddc695dfa2445043e65d8829fbe9f63
Kshitij Pandey <74905584+KSP-07@users.noreply.github.com>
2023-07-04 11:21:13 +0530

    Time: 13 ms (10.58%), Space: 8.6 MB (7.08%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -0,0 +1,21 @@
+
+//https://leetcode.com/problems/counting-bits/discuss/1808016/C%2B%2B-oror-Vectors-Only-oror-Easy-To-Understand-oror-Full-Explanation
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> ans;
+        
+        for(int i = 0 ; i <= n ; i++ ) {
+            int sum = 0 ;
+            int num = i ;
+            
+            while(num) {
+                sum  += num%2 ;
+                num /= 2 ;
+            }
+            
+            ans.push_back( sum ) ;
+        }
+        return ans ;
+    }
+};
