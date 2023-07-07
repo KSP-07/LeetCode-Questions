52fd1a692812d5d4998793b26092ccf19288bbf3
Kshitij Pandey <74905584+KSP-07@users.noreply.github.com>
2023-07-08 05:23:21 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (70.93%) - LeetHub

--- 1646-get-maximum-in-generated-array/1646-get-maximum-in-generated-array.cpp
+++ 1646-get-maximum-in-generated-array/1646-get-maximum-in-generated-array.cpp
@@ -3,15 +3,23 @@ public:
     int getMaximumGenerated(int n) {
         if( n <= 1) return n ;
         vector<int> ans(n+1);
-        
         ans[0] = 0 ;
         ans[1] = 1 ; 
+        /*
         // int maxi = 0;
         for(int i = 1 ; i <= (n/2) ; i++ ) {
             
             ans[2*i] = ans[i] ;
             if(2*i + 1 >n) break;
             ans[2*i + 1 ] = ans[i] + ans[i+1] ;
+        }
+        
+        
+        */
+        for(int i = 2 ; i <= n ; i++ ) {
+            if(i%2 == 0 ) ans[i] = ans[i/2];
+            else ans[i] = ans[i/2] + ans[i/2 + 1];
+            
         }
         int maxi = *max_element(ans.begin() , ans.end()) ;
         
