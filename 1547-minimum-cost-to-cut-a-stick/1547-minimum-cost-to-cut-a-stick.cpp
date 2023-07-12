70bae1743cdf4e6204eaadd581cdbe4bd6af113b
Kshitij Pandey <74905584+KSP-07@users.noreply.github.com>
2023-07-12 06:33:09 +0530

    Time: 107 ms (41.52%), Space: 10.4 MB (20.15%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -1,31 +1,25 @@
 class Solution {
 public:
-    int solve( vector<int> &cuts , vector<vector<int>> &dp , int i , int j){
-        // the smallest rod has length of 1
-        if(j-i <=1 ) return 0;
+    int solve( vector<int> &cuts , vector<vector<int>> &dp , int i , int j ) {
+        if( j - i <= 1 ) return 0;
+        if( dp[i][j] != -1 ) return dp[i][j] ;
         
-        // if the cost is calculated before, return immediately
-        if(dp[i][j]!=-1) return dp[i][j];
-        //we intialise the large number since we are looking for the minimum ans;
-        
-        int ans = 1e9;
-        
-        for(int k = i+1 ; k < j ; k++){
-            // if this rod is to be cut, the cost is the length, 
-            // i.e. cuts[j] - cuts[i]
-            // after the cut, it will be split into two parts
-            // then we calculate the cost for left rod : dfs(cuts, i, k) 
-            // and the cost for the right rod: dfs(cuts, k, j)
-            ans = min(ans , cuts[j] - cuts[i] + solve(cuts , dp , i ,k ) + solve(cuts , dp , k , j));
+        int ans = INT_MAX;
+        for(int k = i+1 ; k < j ; k++ ) {
+            int temp = cuts[j] - cuts[i] + solve(cuts , dp , i , k) + solve(cuts , dp , k , j ) ;
+            
+            ans = min (ans , temp ) ;
         }
-        return dp[i][j] = ans;
+        
+        return dp[i][j] = ans ;
     }
     int minCost(int n, vector<int>& cuts) {
-        cuts.push_back(0);
-        cuts.push_back(n);
-        sort(cuts.begin() , cuts.end());
+        cuts.push_back(0) ;
+        cuts.push_back(n) ;
+        int m = cuts.size() ;
         
-        vector<vector<int>> dp(105 , vector<int> (105 , -1));
-        return solve(cuts , dp , 0 , cuts.size()-1);
+        sort(cuts.begin() , cuts.end()) ;
+        vector<vector<int>> dp( m+1 , vector<int> (m+1 , -1)) ;
+        return solve ( cuts , dp , 0 , m-1) ;
     }
 };
