6bb17a65aa87feb651916b56f681615af729d20f
Kshitij Pandey <74905584+KSP-07@users.noreply.github.com>
2023-07-18 19:35:02 +0530

    Time: 646 ms (24.67%), Space: 22.6 MB (36.01%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -2,16 +2,27 @@ class Solution {
 public:
   
     string longestPalindrome(string S) {
+
         int n = S.size(), maxLen=1, start=0;
-        vector<vector<bool>> dp(n, vector<bool>(n, true));
-        for(int k=1; k<n; k++){    //ye kuu
-            for(int i=0; i+k<n; i++){
-                int j = i + k;
-                if(S[i]==S[j]) dp[i][j] = dp[i+1][j-1];
-                else dp[i][j] = false;
-                if(dp[i][j] && j-i+1>maxLen) maxLen = j-i+1, start = i;
+        vector<vector<bool>> dp(n, vector<bool>(n, false));
+        
+        // for(int i = 0 ; i < n ; i++) dp[i][i] = true;
+        for(int gap = 0; gap < n ; gap++){    
+            for(int i=0 , j = gap ; j < n ; i++ , j++ ){
+                
+                if( gap == 0) dp[i][j] = true;
+                else if ( gap == 1){
+                    if(S[i] == S[j] ) dp[i][j] = true;
+                    else dp[i][j] = false ;
+                }
+                else{
+                    if(S[i] == S[j] && dp[i+1][j-1]) dp[i][j] = true;   //idr check kr rhe ki end ke same and aur fir ha toh unke bich ke same hai ya nhi
+                    else dp[i][j] = false ;
+                }
+                if(dp[i][j] && gap+1 > maxLen) maxLen = gap+1, start = i;
             }
         }
+
         return S.substr(start, maxLen);
     }
 };
