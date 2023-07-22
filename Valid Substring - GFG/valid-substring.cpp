68422eb4c676727061dd7ce6527b955925926313
Kshitij Pandey <74905584+KSP-07@users.noreply.github.com>
2023-07-22 06:24:08 +0530

    Added solution - LeetHub

--- Valid Substring - GFG/valid-substring.cpp
+++ Valid Substring - GFG/valid-substring.cpp
@@ -8,48 +8,28 @@ using namespace std;
 // User function template for c++
 class Solution {
   public:
-    int findMaxLen(string str) {
+    int findMaxLen(string s) {
         // code here
-        int n = str.length();
- 
-    // Create a stack and push -1 as
-    // initial index to it.
-    stack<int> stk;
-    stk.push(-1);
- 
-    // Initialize result
-    int result = 0;
- 
-    // Traverse all characters of given string
-    for (int i = 0; i < n; i++)
-    {
-        // If opening bracket, push index of it
-        if (str[i] == '(')
-            stk.push(i);
-         
-        // If closing bracket, i.e.,str[i] = ')'
-        else
-        {
-            // Pop the previous opening
-            // bracket's index
-            if (!stk.empty())
-            {
-                stk.pop();
+        int cnt = 0 ;
+        stack<int> st;
+        st.push(-1) ;
+        
+        for(int i = 0 ; i< s.size() ; i++ ) {
+            if( s[i] == '(') {
+                st.push( i );
+                // cnt = 0 ;
             }
-             
-            // Check if this length formed with base of
-            // current valid substring is more than max
-            // so far
-            if (!stk.empty())
-                result = max(result, i - stk.top());
- 
-            // If stack is empty. push current index as
-            // base for next valid substring (if any)
-            else
-                stk.push(i);
+            else{
+                if (!st.empty() ) {
+                    st.pop();
+                }
+                if (!st.empty() ) cnt = max (cnt , i - st.top()) ;
+                
+                else st.push(i);
+            }
         }
-    }
-    return result;
+        
+        return cnt ;
     }
 };
 
