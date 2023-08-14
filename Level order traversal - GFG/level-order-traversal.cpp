819256044fc96c2d36ab2633dca5e0d332d8d72d
Kshitij Pandey <74905584+KSP-07@users.noreply.github.com>
2023-08-14 11:15:33 +0530

    Added solution - LeetHub

--- Level order traversal - GFG/level-order-traversal.cpp
+++ Level order traversal - GFG/level-order-traversal.cpp
@@ -45,24 +45,19 @@ class Solution
     vector<int> levelOrder(Node* node)
     {
       //Your code here
-      if(!node) return {-1};
-      
-      vector<int> ans ;
-      queue<Node*> q;
-      q.push(node) ;
-      
-      while(!q.empty()) {
-          auto it = q.front() ; q.pop();
-          ans.push_back(it->data) ;
+      queue<Node *> q;
+      q.push(node);
+      vector<int> ans;
+      while(!q.empty()){
+          Node *curr = q.front();
+          q.pop() ;
           
-          if(it->left){
-              q.push(it->left);
-          }
-          if(it->right){
-              q.push(it->right);
-          }
+          ans.push_back(curr->data);
+          
+          if(curr->left) q.push(curr->left);
+          if(curr->right) q.push(curr->right) ;
+          
       }
-      
       return ans;
     }
 };
