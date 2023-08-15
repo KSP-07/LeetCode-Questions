f203448d8a0153aaa3efbe0a215215ad44a55270
Kshitij Pandey <74905584+KSP-07@users.noreply.github.com>
2023-08-15 17:18:22 +0530

    Added solution - LeetHub

--- Normal BST to Balanced BST - GFG/normal-bst-to-balanced-bst.cpp
+++ Normal BST to Balanced BST - GFG/normal-bst-to-balanced-bst.cpp
@@ -139,34 +139,39 @@ struct Node
 */
 // Your are required to complete this function
 // function should return root of the modified BST
-void inorder(Node *root , auto &ans){
-    if(!root) return;
-    inorder(root->left,ans);
-    ans.push_back(root->data);
-    inorder(root->right,ans);
-}
 
-//mid element leke bst bana rahe to dono side se balanced bann jayga
-Node *toBst(auto &ans , int s , int e){
-    if(s>e) return NULL;
-    int mid = s + (e-s)/2;
+
+Node *toBST(vector<int> &ans , int s , int e){
+    if( s>e) return NULL;
     
-    Node *temp = new Node(ans[mid]);
-    temp->left = toBst(ans , s , mid-1);
-    temp->right =toBst(ans , mid+1,e);
+    int mid = s + (e - s)/2 ;
+    
+    Node *temp = new Node(ans[mid]) ;
+    
+    temp->left = toBST(ans , s , mid - 1) ;
+    temp->right = toBST(ans , mid + 1 , e ) ;
     
     return temp;
 }
 
+ void inorder(Node *root , vector<int> &ans) {
+     if(!root) return ;
+     
+     inorder(root->left , ans) ;
+     ans.push_back(root->data) ;
+     inorder(root->right , ans) ;
+ }
+
 Node* buildBalancedTree(Node* root)
 {
 	// Code here
-	if(!root) return NULL;
+	if(!root)  return NULL ;
+	
 	vector<int> ans;
-	//sorted order store krlege array mai , then mid element se bst bnane lagage to balanced aayga
-	inorder(root , ans);
-	int n=ans.size();
-	Node *final_ans=toBst(ans, 0 , n-1);
+	inorder(root , ans );
+	int n = ans.size();
+	
+	Node *final_ans = toBST(ans , 0 , n-1);
 	
 	return final_ans;
 }
