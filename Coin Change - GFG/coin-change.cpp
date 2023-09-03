c3f0bcba816545e5a809d298539b9b1e90c68089
Kshitij Pandey <74905584+KSP-07@users.noreply.github.com>
2023-09-03 17:57:45 +0530

    Added solution - LeetHub

--- Coin Change - GFG/coin-change.cpp
+++ Coin Change - GFG/coin-change.cpp
@@ -5,48 +5,28 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-//   long long solve(int coins[]  , int n , int sum , vector<vector<long long>> &dp){
-//       if(sum==0){
-//           return 1;
-//       }
-//       if(n==0){
-//         //   dp[n]
-//           return 0;
-//       }
-//       if(dp[n][sum]!=-1) return dp[n][sum];
-      
-//       if(coins[n-1]<=sum){
-//           return dp[n][sum] = solve(coins , n-1 , sum , dp) + solve(coins,n , sum-coins[n-1] , dp);
-//       }
-//       else return dp[n][sum] = solve(coins , n-1 , sum , dp);
-//   }
     long long int count(int coins[], int N, int sum) {
 
         // code here.
-        vector<vector<long long>> dp(N+1 , vector<long long> (sum+1 ,0));
-        int m = sum+1;
-        //  for(int i=0,j=0;i<m;i++) dp[j][i]=0;
-        // for(int i=0,j=0;i<N+1;i++) dp[i][j]=1;
-       for(int t=0;t<=sum;t++)
-        {
-            dp[0][t]=(t%coins[0]==0);
+        
+        vector<vector<long long>> dp(N+1 , vector<long long> (sum+1 , 0)) ;
+        
+        for(int i = 0 ; i <= N ; i++ ){
+            dp[i][0] = 1;
         }
-        for(int ind=1;ind<N;ind++)
-        {
-            for(int t=0;t<=sum;t++)
-            {
-                long long int notTake=dp[ind-1][t];
-                long long int take=0;
-                if(coins[ind]<=t)
-                {
-                    take=dp[ind][t-coins[ind]];
+        
+        for(int i = 1 ; i <= N ; i++){
+            for(int j = 1 ; j <= sum ; j++){
+                
+                if(coins[i-1] <= j ){
+                    dp[i][j] = dp[i][j - coins[i-1]] + dp[i-1][j];
                 }
-                dp[ind][t]=take+notTake;
+                else dp[i][j] = dp[i-1][j] ;
             }
         }
-        return dp[N-1][sum];
         
-        // return solve(coins , N, sum , dp);
+        return dp[N][sum];
+        
     }
 };
 
