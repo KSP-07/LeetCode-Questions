9f49d4f8d9416ba1f308f0cc32ed635a39780fa7
Kshitij Pandey <74905584+KSP-07@users.noreply.github.com>
2023-09-13 18:34:08 +0530

    Added solution - LeetHub

--- Longest Palindromic Subsequence - GFG/longest-palindromic-subsequence.cpp
+++ Longest Palindromic Subsequence - GFG/longest-palindromic-subsequence.cpp
@@ -10,36 +10,42 @@ using namespace std;
 
 class Solution{
   public:
-  bool isPalindrome( string &s , int i , int j){
+  bool isPalindrome( string &s , int i , int j ){
       while( i < j ) {
-          if(s[i] != s[j]) return false;
+          if(s[i] != s[j] ) return false;
+          
           i++;
           j--;
       }
       return true;
   }
   
-  int f(string &s , vector<vector<int>> &dp , int i , int j) {
-      if( i>j) return 0 ;
+  int solve(string &str , vector<vector<int>> &dp , int i , int j ) {
+      if( i > j ) return 0 ;
+      
       if(dp[i][j] != -1 ) return dp[i][j] ;
       
-      if( isPalindrome(s , i , j )){
-          return dp[i][j] = (j-i+1);
+      if(isPalindrome(str , i , j )){
+          return dp[i][j] = ( j - i + 1 ) ; 
       }
-      int nopick = 0 , pick = 0;
-      if(s[i] == s[j]){
-          pick = 2 + f(s , dp , i+1 , j-1);
-      }
-      nopick = max( f(s , dp , i+1 , j) , f(s , dp , i , j-1) );
       
-      return dp[i][j] = max(pick , nopick);
+      int nopick = 0 , pick = 0 ;
+      
+      if(str[i] == str[j]){
+          pick = 2 + solve(str , dp , i+1 , j-1 ) ;
+      }
+      
+      nopick = max( solve(str , dp , i+1 , j) , solve(str , dp , i , j-1));
+      
+      return dp[i][j] = max(pick , nopick ) ;
+      
   }
     int longestPalinSubseq(string A) {
         //code here
         int n = A.size() ;
         vector<vector<int>> dp (n , vector<int> (n , -1)) ;
         
-        return f(A , dp , 0 , n-1) ;
+        return solve(A , dp , 0 , n-1);
     }
 };
 
